0afebfb3356d025da0c6fd0177703fca8ac6ce67
macdfree <macdfree@163.com>
2018-01-18 23:19:33 +0800

    修改单元测试

--- crypto/jksutil_test.go
+++ crypto/jksutil_test.go
@@ -5,5 +5,8 @@ import (
 )
 
 func TestJksUtil(t *testing.T) {
-	CreateJks("www.baidu.com", []string{"test_root.crt", "test_server.crt"}, "test_server.key", "test.jks", "123456")
+	err := CreateJks("www.baidu.com", []string{"test_root.crt", "test_server.crt"}, "test_server.key", "test.jks", "123456")
+	if err != nil {
+		t.Fatal("创建jks文件报错", err)
+	}
 }
