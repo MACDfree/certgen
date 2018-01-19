3acd0a9d8a9f341046c5ba055115a7ac578d1405
macdfree <macdfree@163.com>
2018-01-19 23:13:40 +0800

    杂项

---
+++ ui/webui.go
@@ -0,0 +1,15 @@
+package ui
+
+import (
+	"net/http"
+)
+
+// StartHTTP 启动http服务
+func StartHTTP(addr string) {
+	http.ListenAndServe(addr, nil)
+}
+
+// 生成证书对
+func keypair(w http.ResponseWriter, r *http.Request) {
+
+}
